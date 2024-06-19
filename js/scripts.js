a91109728738d5252279598753b885421f291ea4
Jacqueson <jacquesonbenevides@gmail.com>
2024-06-18 23:19:55 -0300

    add scripts js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,61 @@
-// Elements
 
-// Functions
+function writeOnDisplay(value) {
+    display.value += value;
+}
 
-// Events
+function operation(op) {
+    if (operand1 === null) {
+        operand1 = parseFloat(display.value);
+        operator = op;
+        display.value = '';
+    } else {
+        operand2 = parseFloat(display.value);
+        calculate();
+        operator = op;
+    }
+}
+
+function calculate() {
+    if (operand1 !== null && operand2 !== null && operator !== null) {
+        switch (operator) {
+            case '+':
+                display.value = operand1 + operand2;
+                break;
+            case '-':
+                display.value = operand1 - operand2;
+                break;
+            case '*':
+                display.value = operand1 * operand2;
+                break;
+            case '/':
+                if (operand2 === 0) {
+                    display.value = 'Error';
+                } else {
+                    display.value = operand1 / operand2;
+                }
+                break;
+            default:
+                display.value = 'Error';
+                break;
+        }
+        operand1 = parseFloat(display.value);
+        operand2 = null;
+    }
+}
+
+function calculateResult() {
+    operand2 = parseFloat(display.value);
+    calculate();
+    operator = null;
+}
+
+function clearAll() {
+    clearDisplay();
+    operand1 = null;
+    operand2 = null;
+    operator = null;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
